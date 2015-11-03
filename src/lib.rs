93b0651194c40e8f00f325da4c7468760a6ffdef
Rafal Mielniczuk <rafal.mielniczuk@citrix.com>
2015-11-03 09:59:23 +0000

    Add missing files

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+#![feature(no_std, lang_items)]
+#![feature(const_fn, unique, core_str_ext)]
+#![no_std]
+
+extern crate rlibc;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
+
+#[no_mangle]
+pub extern fn rust_main() {
+    // ATTENTION: we have a very small stack and no guard page
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
+
+    loop{}
+}
+
+#[cfg(not(test))]
+#[lang = "eh_personality"]
+extern fn eh_personality() {}
+
+#[cfg(not(test))]
+#[lang = "panic_fmt"]
+extern fn panic_fmt() -> ! {loop{}}
