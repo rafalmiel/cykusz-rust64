87ebff08d67cd9ba6c997ca93f3fc955b0f39002
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2015-11-30 22:42:43 +0000

    remove build dir

--- update_core_nightly.sh
+++ update_core_nightly.sh
@@ -4,7 +4,9 @@ multirust update nightly
 multirust override nightly
 pushd .
 cd ../rust
+git stash
 git pull
+git stash pop
 popd
 mkdir -p ~/.multirust/toolchains/nightly/lib/rustlib/x86_64-unknown-none-gnu/lib
 rustc --target x86_64-unknown-none-gnu -Z no-landing-pads --out-dir ~/.multirust/toolchains/nightly/lib/rustlib/x86_64-unknown-none-gnu/lib ../rust/src/libcore/lib.rs
