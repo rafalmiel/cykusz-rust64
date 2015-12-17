bde6b6a93d0e3869ff94d346d94f3dd455db51c9
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2015-12-17 19:12:47 +0000

    Add git submodule update for the rust repo

--- update_core_nightly.sh
+++ update_core_nightly.sh
@@ -5,6 +5,7 @@ multirust override nightly
 pushd .
 cd ../rust
 git pull
+git submodule update
 popd
 mkdir -p ~/.multirust/toolchains/nightly/lib/rustlib/x86_64-unknown-none-gnu/lib
 rustc --target x86_64-unknown-none-gnu -Z no-landing-pads --out-dir ~/.multirust/toolchains/nightly/lib/rustlib/x86_64-unknown-none-gnu/lib ../rust/src/libcore/lib.rs
